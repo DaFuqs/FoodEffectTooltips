dc1578a21b58f84f3ae250c0f15eb3d654488eb6
DaFuqs <dominik.m.schmidt@googlemail.com>
2022-12-28 11:55:46 +0100

    updated to 1.19.3. Added config option to show suspicious stew effects

--- src/main/java/de/dafuqs/foodeffecttooltips/FoodeffecttooltipsClient.java
+++ src/main/java/de/dafuqs/foodeffecttooltips/FoodeffecttooltipsClient.java
@@ -7,10 +7,20 @@ import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.client.item.v1.ItemTooltipCallback;
+import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.item.SuspiciousStewItem;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.nbt.NbtList;
+import net.minecraft.potion.PotionUtil;
+import net.minecraft.registry.Registries;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Environment(EnvType.CLIENT)
 public class FoodeffecttooltipsClient implements ClientModInitializer {
@@ -24,7 +34,12 @@ public class FoodeffecttooltipsClient implements ClientModInitializer {
 		CONFIG = AutoConfig.getConfigHolder(FoodEffectsConfig.class).getConfig();
 		
 		ItemTooltipCallback.EVENT.register((stack, context, lines) -> {
-			if(stack.isFood() && shouldShowTooltip(stack)) {
+			if(stack.isOf(Items.SUSPICIOUS_STEW) && FoodeffecttooltipsClient.CONFIG.ShowSuspiciousStewTooltips && !context.isCreative()) {
+				List<StatusEffectInstance> effects = getStewEffects(stack);
+				if(effects.size() > 0) {
+					PotionUtil.buildTooltip(effects, lines, 1.0F);
+				}
+			} else if(stack.isFood() && shouldShowTooltip(stack)) {
 				TooltipHelper.addFoodComponentEffectTooltip(stack, lines);
 			}
 		});
@@ -36,7 +51,7 @@ public class FoodeffecttooltipsClient implements ClientModInitializer {
 		}
 		
 		Item item = stack.getItem();
-		Identifier identifier = Registry.ITEM.getId(item);
+		Identifier identifier = Registries.ITEM.getId(item);
 		
 		boolean isWhitelist = CONFIG.UseAsWhitelistInstead;
 		if(CONFIG.BlacklistedItemIdentifiers.contains(identifier.toString())) {
@@ -46,6 +61,28 @@ public class FoodeffecttooltipsClient implements ClientModInitializer {
 			return isWhitelist;
 		}
 		return !isWhitelist;
+	}
+	
+	private static List<StatusEffectInstance> getStewEffects(ItemStack stew) {
+		List<StatusEffectInstance> effects = new ArrayList<>();
+		NbtCompound nbtCompound = stew.getNbt();
+		if (nbtCompound != null && nbtCompound.contains(SuspiciousStewItem.EFFECTS_KEY, 9)) {
+			NbtList nbtList = nbtCompound.getList(SuspiciousStewItem.EFFECTS_KEY, 10);
+			
+			for(int i = 0; i < nbtList.size(); ++i) {
+				NbtCompound nbtCompound2 = nbtList.getCompound(i);
+				int j = 160;
+				if (nbtCompound2.contains(SuspiciousStewItem.EFFECT_DURATION_KEY, 3)) {
+					j = nbtCompound2.getInt(SuspiciousStewItem.EFFECT_DURATION_KEY);
+				}
+				
+				StatusEffect statusEffect = StatusEffect.byRawId(nbtCompound2.getInt(SuspiciousStewItem.EFFECT_ID_KEY));
+				if (statusEffect != null) {
+					effects.add(new StatusEffectInstance(statusEffect, j));
+				}
+			}
+		}
+		return effects;
 	}
 	
 	
